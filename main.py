5080a4215b0f139ec32b5d80293ebdc4183a5518
ErikLz00 <113318550+ErikLz00@users.noreply.github.com>
2022-09-17 04:26:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,151 +1,152 @@
 import time
+import random
+from os import system
 
-BLACK='\033[3Om'
-RED='\033[31m'
-GREEN ='\033[32m'
-YELLOW='\033[33m'
-BLUE ='\033[34m'
-MAGENTA ='\033[35m'
-CYAN ='\033[36m'
-WHITE ='\033[37m'
-RESET ='\033[39m'
 
-rpta=['a','b','c','d']
+#colores
+BLACK = "\033[0;30m"
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+BROWN = "\033[0;33m"
+BLUE = "\033[0;34m"
+PURPLE = "\033[0;35m"
+CYAN = "\033[0;36m"
+LIGHT_GRAY = "\033[0;37m"
+DARK_GRAY = "\033[1;30m"
+LIGHT_RED = "\033[1;31m"
+LIGHT_GREEN = "\033[1;32m"
+YELLOW = "\033[1;33m"
+LIGHT_BLUE = "\033[1;34m"
+LIGHT_PURPLE = "\033[1;35m"
+LIGHT_CYAN = "\033[1;36m"
+LIGHT_WHITE = "\033[1;37m"
+BOLD = "\033[1m"
+FAINT = "\033[2m"
+ITALIC = "\033[3m"
+UNDERLINE = "\033[4m"
+BLINK = "\033[5m"
+NEGATIVE = "\033[7m"
+CROSSED = "\033[9m"
+END = "\033[0m"
+RESET ="\033[39m"
 
-iniciarTrivia=True
+#creando una lista con las preguntas
+preguntas=['1) ¿Quién fue el creador de Linux ?',
+          '2) ¿Cuál de estos no es un navegador ?',
+          '3) ¿Cuál de la siguiente opciones no es un motor de búsqueda ?',
+          '4) ¿Cuál de estos programas no esta en Microsoft Office?',
+          '5) ¿Qué significa WI-FI?',
+          ]
 
-nombre=input("Ingrese su nombre: ")
+#creando una lista con las alternativas
+contenido=['\na) Linus Torvalds \nb) Bill Gates \nc) Mark Sukerberg \nd) Dennis Ritchie ',
+          '\na) Mozilla \nb) Chrome \nc) Opera \nd) Turbonet',
+          '\na) Yahoo \nb) Google \nc) Oracle \nd) Bing',
+          '\na) Word \nb) Excel \nc) Paint \nd) PowerPoint',
+          '\na) Wiggly Fibres \nb) Wireless Fidelity \nc) Wireless Fings \nd) Width of File'
+          ]
 
-print("==== Bienvenido ", nombre.upper()," a mi trivia sobre Informatica ==== ")
-  
-print("-Responde las siguientes preguntas escribiendo la letra correcta ")
-print("-Empezaras con un puntaje de 0 ")
-print("-Respuestas correctas +10 puntos e incorrectas -5 puntos")
+#lista con las respuestas 
+respuestas=['a','d','c','c','b']
 
-while iniciarTrivia==True:
+claves=['a','b','c','d']
 
+#variable de condicion para que inicie el bucle
+iniciatrivia=True
+
+system("cls")
+
+#iniciamos pidiendo el nombre del participante
+nombre=input(LIGHT_BLUE+"\nINGRESE SU NOMBRE : "+RESET)
+
+time.sleep(2)
+
+system("cls")
+
+#le damos la bienvenida y mencionamos la regla del juego
+print(LIGHT_GREEN+"\n==== BIENVENIDO ", nombre.upper()," A LA TRIVIA DE INFORMATICA ===="+RESET)
+print(LIGHT_WHITE+"\n-Responde las siguientes preguntas escribiendo la letra correcta ")
+print("\n-Empezaras con un puntaje de 0 ")
+print("\n-Respuestas correctas se agregaran puntos y las incorrectas se quitaran puntos")
+print("\n-Los puntos se daran aleatoriamente "+RESET)
+
+time.sleep(6) 
+
+system("cls")
+
+#creamos un bucle para comenzar y si quiere seguir jugando
+while iniciatrivia==True:
+
+  #iniciamos el puntaje en cero
   puntaje=0
+  
+  #variable que recorrera la lista
+  inicio=0
 
-  print("\n=========COMENZEMOS=========\n")
-  time.sleep(2)
-  
-  print("1) Quién fue el creador de Linux ?\n")
-  
-  print("a) Linus Torvalds")
-  print("b) Bill Gates")
-  print("c) Mark Sukerberg")
-  print("d) Dennis Ritchie")
-  
-  respuesta_1=input("Ingrese su respuesta : ")
+  print(LIGHT_PURPLE+"\n=========  COMENZEMOS  =========\n"+RESET)
 
-  while respuesta_1 not in rpta:
-    respuesta_1=input("Ingrese la letra correcta : ")
-  time.sleep(2)
-  if respuesta_1 == 'a' :
-    print (GREEN+"Correcto"+RESET)
-    puntaje=puntaje+10
-  else:
-    print(RED+"Incorrecto"+RESET)
-    puntaje=puntaje-5
-  
-  time.sleep(2)  
-  
-  print("\n2) Cuál de estos no es un navegador ?")
-  
-  print("a) Mozilla")
-  print("b) Chrome")
-  print("c) Opera")
-  print("d) Turbonet")
-  
-  respuesta_2=input("Ingrese su respuesta : ")
-  while respuesta_1 not in rpta:
-    respuesta_1=input("Ingrese la letra correcta : ")
-  time.sleep(2)  
-  
-  if respuesta_2.lower() == 'd' :
-    print (GREEN+"Correcto"+RESET)
-    puntaje=puntaje+10
-  else:
-    print(RED+"Incorrecto"+RESET)
-    puntaje=puntaje-5
-  
-  time.sleep(2)  
-  
-  print("\n3) Cuál de la siguiente opciones no es un motor de búsqueda ?")
-  
-  print("a) Yahoo")
-  print("b) Google")
-  print("c) Oracle")
-  print("d) Bing")
-  
-  respuesta_3=input("Ingrese su respuesta : ")
-  while respuesta_1 not in rpta:
-    respuesta_1=input("Ingrese la letra correcta : ")
   time.sleep(2)
   
-  if respuesta_3.lower() == 'c' :
-    print (GREEN+"Correcto"+RESET)
-    puntaje=puntaje+10
-  else:
-    print(RED+"Incorrecto"+RESET)
-    puntaje=puntaje-5
-  
-  time.sleep(2)  
-  
-  print("\n4) Cuál de estos programas no esta en Microsoft Office? ")
-  
-  print("a) Word")
-  print("b) Excel")
-  print("c) Paint")
-  print("d) PowerPoint")
-  
-  respuesta_4=input("Ingrese su respuesta : ")
-  while respuesta_1 not in rpta:
-    respuesta_1=input("Ingrese la letra correcta : ")
-  time.sleep(2)
-  
-  if respuesta_4.lower() == 'c' :
-    print (GREEN+"Correcto"+RESET)
-    puntaje=puntaje+10
-  else:
-    print(RED+"Incorrecto"+RESET)
-    puntaje=puntaje-5
+  while inicio<len(preguntas):
+
+    #mostramos las preguntas y alternativas mostrando la lista
+    print(preguntas[inicio])
+    print(contenido[inicio])
+
+    #pedimos que ingrese la respuesta
+    respuesta_1=input(BLUE+"\nRespuesta : "+RESET)
+
+    time.sleep(2)
+
+    while respuesta_1 not in claves:
+
+      #si ingresa otra clave, le pedimos que ingrese de nuevo 
+      print(YELLOW+"\nIngrese de nuevo la clave ")
+      respuesta_1=input(BLUE+"\nRespuesta : "+RESET)
+
+    if respuestas[inicio] == respuesta_1.lower() :
+
+      #mostramos la alerta si la respuesta es correcta
+      print (GREEN+"\nCORRECTO"+RESET)
+      puntaje=puntaje+random.randint(1,10)
     
-  time.sleep(2)  
-  
-  print("\n5) Qué significa WI-FI? ")
-  
-  print("a) Wiggly Fibres")
-  print("b) Wireless Fidelity")
-  print("c) Wireless Fings")
-  print("d) Width of File")
-  
-  respuesta_5=input("Ingrese su respuesta : ")
-  while respuesta_1 not in rpta:
-    respuesta_1=input("Ingrese la letra correcta : ")
-  time.sleep(2)
+    else:
+      
+      #mostramos la alerta si la respuesta es incorrecta
+      print(RED+"\nINCORRECTO"+RESET)
+      puntaje=puntaje-random.randint(1,10)
     
-  if respuesta_5.lower() == 'b' :
-    print (GREEN+"Correcto"+RESET)
-    puntaje=puntaje+10
-  else:
-    print(RED+"Incorrecto"+RESET)
-    puntaje=puntaje-5
-    
-  time.sleep(2)
+    time.sleep(2) 
 
-  print("---La trivia ha finalizado---")
+    system("cls") 
+    inicio+=1
+
+  #mostramos un mensaje cuando se termina las preguntas
+  print(CYAN+"----- LA TRIVIA HA FINALIZADO -----\n"+RESET)
 
   time.sleep(2)
   
+  #mostramos el puntaje obtenido del participante
   print(nombre.upper()," haz obtenido ",puntaje," puntos")
 
-  print("\n Deseas realizar la trivia otravez ?")
-  opcion=input("1)si    2)no").lower()
+  #consultamos si quiere realizar de nuevo la TRIVIA
+  print("\nDeseas realizar la trivia otravez ?")
+  opcion=input("\n1)si         2)no    : ").lower()
 
-  if opcion=='no':
-    print("Espero que lo hayas pasado bien, hasta pronto! ")
-    iniciarTrivia=False
-    
-  else:
+  system("cls")
+
+  if opcion=='si':
+
+    #si elije realizar otra trivia, reiniciamos el puntaje a cero y la variable que recorrera la lista
     puntaje=0
+
+    inicio=0
+
+  else:
+
+    #si no quiere realizar otra trivia le mostramos un mensaje  
+    #igualamos a false la condicion para que se cierre el bucle
+
+    print(CYAN+"\nGracias por realizar la Trivia, hasta pronto! \n"+RESET)
+    
+    iniciatrivia=False
